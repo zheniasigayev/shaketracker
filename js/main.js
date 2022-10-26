08b630ff43e04f8898246b2d85ed3528ac20a276
Zhenia Sigayev <58703868+zheniasigayev@users.noreply.github.com>
2022-10-25 22:46:03 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,11 +16,6 @@ csvFileInput.addEventListener("change", e => {
             let temp = []
             for (let i = 1; i < results.data.slice(1,56).length; i++) {
 
-                // Unformatted dates
-                //cpy.push(results.data[i].slice(0,6))
-                //console.log(results.data[i].slice(0,6))
-
-
                 // Workaround to format date                
                 temp.push(results.data[i].slice(0,1))
                 temp.push([results.data[i][1].slice(0,10)]) //Convert from ISO long to 
@@ -30,25 +25,7 @@ csvFileInput.addEventListener("change", e => {
                 temp.push(results.data[i].slice(5,6))
 
                 cpy.push(temp)
-
                 temp = []
-
-
-
-
-                //cpy.push(results.data[i].slice(0,6))
-                //let dd = results.data[i][1]
-                //dd = dd.slice(0,10)
-                //cpy.push(dd)
-                //cpy.push(results.data[i].slice(2,6))
-                //cpy[i][1] = results.data[i][0].slice(0,10)
-                //console.log(cpy[0][1])
-                //console.log(results.data[i][1].slice(0,10))
-                
-
-
-
-
             }
 
             tableCsv.update(cpy, results.data[0].slice(0,6)) // 1st param = body, 2nd = header
